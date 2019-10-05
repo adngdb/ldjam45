cf965d707c6b7ddf2c33dc887e7cc9e99d442e1f
Staś Małolepszy <stas@piesku.com>
2019-10-05 17:38:43 +0200

    Add State singleton

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,4 +3,5 @@ extends RigidBody
 export var force: float
 
 func _physics_process(delta):
-	add_central_force(Vector3(force, 0, 0))
+	if not State.rules_active.has(State.NOTHING_MOVES):
+		add_central_force(Vector3(force, 0, 0))
---
+++ Scripts/State.gd
@@ -0,0 +1,8 @@
+extends Node
+
+enum {
+	NOTHING_MOVES,
+}
+
+var rules_available = [NOTHING_MOVES]
+var rules_active = [NOTHING_MOVES]
