1c3377b3f39656063647a5146b88eb2d4886880e
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-05 18:21:26 +0200

    Add set_rule option for drag and drop, use in operator.

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -8,15 +8,19 @@ var rules_available = [NOTHING_MOVES]
 var rules_active = []
 
 func toggle_rule(rule):
-	if rules_active.has(rule):
+	if rule in rules_active:
 		self.deactivate_rule(rule)
 	else:
 		self.activate_rule(rule)
 
 func activate_rule(rule):
-	if not rules_active.has(rule):
+	if not rule in rules_active:
 		rules_active.append(rule)
 
 func deactivate_rule(rule):
-	if rules_active.has(rule):
+	if rule in rules_active:
 		rules_active.remove(rules_active.find(rule))
+
+func set_rule(index, rule):
+	if not rule in rules_active:
+		rules_active[index] = rule
