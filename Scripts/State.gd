db4ae629bb30d8b7473e5687cf63539654972f81
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-05 18:01:27 +0200

    Enable toggling moving state.

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -7,5 +7,16 @@ enum {
 var rules_available = [NOTHING_MOVES]
 var rules_active = []
 
-func select_rule(rule):
-	rules_active = [rule]
+func toggle_rule(rule):
+	if rules_active.has(rule):
+		self.deactivate_rule(rule)
+	else:
+		self.activate_rule(rule)
+
+func activate_rule(rule):
+	if not rules_active.has(rule):
+		rules_active.append(rule)
+
+func deactivate_rule(rule):
+	if rules_active.has(rule):
+		rules_active.remove(rules_active.find(rule))
