81060547d43959b71e3a09911e9e92773dd56066
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-05 22:38:17 +0200

    Make bigger buttons.

--- Scripts/Rules.gd
+++ Scripts/Rules.gd
@@ -1,10 +1,13 @@
 extends Panel
 
+const BASE_SIZE = 70
+
 func _ready():
 	var count = 0
 	for rule in State.rules_available:
 		var data = State.RULES_DATA[rule]
 		var word = WordDragable.new(rule, data.word)
 		add_child(word)
-		word.set_position(Vector2(count * 100 + 50, 50))
+		word.set_position(Vector2(count * BASE_SIZE * 3 + BASE_SIZE, (200 - BASE_SIZE) / 2))
+		word.rect_size = Vector2(BASE_SIZE * 2, BASE_SIZE)
 		count += 1
