3289a9c743c7d762a2d99d28e87a38da24cb6719
Staś Małolepszy <stas@piesku.com>
2019-10-05 20:56:13 +0200

    Detect collisions also when colliders keep overlapping

--- Scripts/Element.gd
+++ Scripts/Element.gd
@@ -1,9 +1,14 @@
 extends Area
 
 export (State.Rule) var rule
+var colliding_bodies = []
 
 func _on_body_entered(body):
-	print(body)
-	
-	if not rule in State.rules_active:
+	colliding_bodies.append(body)
+
+func _on_body_exited(body):
+	colliding_bodies.remove(colliding_bodies.find(body))
+
+func _process(delta):
+	if colliding_bodies.size() > 0 and not rule in State.rules_active:
 		State.kill_player()
