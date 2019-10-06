308c4bcf072501f8f2b5b6150237b64d2804e754
Staś Małolepszy <stas@piesku.com>
2019-10-06 20:03:36 +0200

    Play Die when the Player dies

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,20 @@
 extends KinematicBody
 
 export var motion: Vector3
+var is_alive = true
 
-func _physics_process(delta):
-	move_and_collide(delta * motion)
-
-func _process(delta):
+func _ready():
 	$AnimationPlayer.play("Run")
+	
+func _physics_process(delta):
+	if is_alive:
+		move_and_collide(delta * motion)
+
+func kill():
+	is_alive = false
+	$AnimationPlayer.play("Die")
+	$CollisionShape.disabled = true
+
+func _on_animation_finished(anim_name):
+	if anim_name == "Die":
+		State.kill_player()
