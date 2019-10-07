389f58a6a338aa02cd49dcd367a3a3bb69f878dc
Staś Małolepszy <stas@piesku.com>
2019-10-07 20:49:40 +0200

    Add spotlights pointing up to all obstacles

--- Scripts/Element.gd
+++ Scripts/Element.gd
@@ -2,6 +2,7 @@ extends Area
 
 export (State.Rule) var rule
 var colliding_bodies = []
+onready var light = $SpotLight
 
 func _on_body_entered(body):
 	colliding_bodies.append(body)
@@ -13,9 +14,15 @@ func _on_body_exited(body):
 		SuccessSound.play()
 
 func _process(delta):
-	if colliding_bodies.size() > 0 and not rule in State.rules_active:
-		var body = colliding_bodies[0]
-		if body.is_alive:
-			body.hurt(delta)
-			if not body.is_alive and $DeathSound:
-				$DeathSound.play()
+	if rule in State.rules_active:
+		if light:
+			light.visible = false
+	else:
+		if light:
+			light.visible = true
+		if colliding_bodies.size() > 0:
+			var body = colliding_bodies[0]
+			if body.is_alive:
+				body.hurt(delta)
+				if not body.is_alive and $DeathSound:
+					$DeathSound.play()
