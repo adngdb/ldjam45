8a560321281ff17eda126de10fa1287e716b8f8e
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-07 15:43:56 +0200

    Show key in UI.

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -35,11 +35,11 @@ const RULES_DATA = {
 var rule_slots = 1
 
 var rules_available = [
-	#Rule.NOTHING_BURNS,
-	#Rule.NOTHING_DROWNS,
-	#Rule.NOTHING_STINGS,
-	#Rule.NOTHING_GRABS,
-	#Rule.NOTHING_FLIES,
+	# Rule.NOTHING_BURNS,
+	# Rule.NOTHING_DROWNS,
+	# Rule.NOTHING_STINGS,
+	# Rule.NOTHING_GRABS,
+	# Rule.NOTHING_FLIES,
 ]
 var rules_active = []
 
